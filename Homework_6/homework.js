6156ff3d90f3b13936281b6ea1146ac649fe8947
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-04-05 16:45:29 +0300

    some changes

--- Homework_6/homework.js
+++ Homework_6/homework.js
@@ -13,13 +13,13 @@ const showAge = (qBirthDate && Number(qBirthDate) && (Number(qBirthDate) < 2024)
 							? 2024 - qBirthDate
 							: 'Canceled or invalid input';
 
-if (!qBirthPlace) showBirthPlace = 'Canceled or invalid input';
+if (!qBirthPlace) showBirthPlace = 'Canceled or empty input';
 else if (qBirthPlace.toLowerCase() === 'kyiv') showBirthPlace = 'Y live in UKRAINE';
 else if (qBirthPlace.toLowerCase() === 'london') showBirthPlace = 'Y live in England';
 else if (qBirthPlace.toLowerCase() === 'washington') showBirthPlace = 'Y live in USA';
 else showBirthPlace = `Y live in ${qBirthPlace} city`;
 
-if (!qFavoriteSport) showSports = 'Canceled or invalid input';
+if (!qFavoriteSport) showSports = 'Canceled or empty input';
 else if (qFavoriteSport.toLowerCase() === 'boxing') showSports = `Cool! Y wana be an Usik?`;
 else if (qFavoriteSport.toLowerCase() === 'swimming') showSports = `Cool! Y wana be an Phelps?`;
 else if (qFavoriteSport.toLowerCase() === 'tennis') showSports = `Cool! Y wana be an Williams?`;
