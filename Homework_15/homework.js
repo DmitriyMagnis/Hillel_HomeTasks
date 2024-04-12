bb64af155b7382e1ffd642da680026131c3d2bd8
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-04-12 20:57:28 +0300

    same changes

--- Homework_15/homework.js
+++ Homework_15/homework.js
@@ -2,6 +2,9 @@ const array = [1, 3, 4, 6, 2, 5, 7];
 
 function removeElement(array = [], item) {
   const itemIndex = array.indexOf(item);
+
+  if (itemIndex < 0) return array;
+
   const arrayWithoutItem = array.splice(itemIndex, 1);
   return arrayWithoutItem;
 }
