c8b7873337251a924a734ee9e92d332f2c3dadac
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-04-16 16:20:44 +0300

    added task cancelation

--- Homework_18/homework.js
+++ Homework_18/homework.js
@@ -1,9 +1,11 @@
 function printChoise() {
-  let input = null;
   for (let i = 1; i <= 10; i++) {
-    input = prompt(`Enter number greater then 100: Attempt ${i}`);
-
-    //Number(null, '' etc)=> 0 NaN < 100
+    const input = prompt(`Enter number greater then 100: Attempt ${i}`);
+    if (input === null) {
+      console.log('Task canceled');
+      break;
+    }
+    //Number(null, '' etc) => 0 NaN < 100
     if (input > 100) {
       console.log('Number: ' + input);
       console.log('Attempt: ' + i);
