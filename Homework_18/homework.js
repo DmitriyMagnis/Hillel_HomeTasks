86706777cd3ad72609c6fbf81f59c25d2b67ca94
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-04-16 16:15:43 +0300

    changed start counter

--- Homework_18/homework.js
+++ Homework_18/homework.js
@@ -1,10 +1,12 @@
 function printChoise() {
   let input = null;
-  for (let i = 0; i < 10; i++) {
-    input = prompt(`Enter number greater then 100: Attempt ${i + 1}`);
+  for (let i = 1; i <= 10; i++) {
+    input = prompt(`Enter number greater then 100: Attempt ${i}`);
+
+    //Number(null, '' etc)=> 0 NaN < 100
     if (input > 100) {
       console.log('Number: ' + input);
-      console.log('Attempt: ' + (i + 1));
+      console.log('Attempt: ' + i);
       break;
     }
   }
