663f56f9df5f93ca050ab4c3c1dc89e3c1c96cbd
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-05-03 01:00:04 +0300

    hw_27 changes

--- Homework_27/homework.js
+++ Homework_27/homework.js
@@ -1,2 +1,31 @@
-const text = document.querySelector('.text');
-const btn = document.querySelector('.btn');
+const linkBtn = document.querySelector('.link');
+const redirectBtn = document.querySelector('.redirect');
+
+class StateManager {
+  constructor(initialState = null) {
+    this._state = initialState ?? structuredClone(initialState);
+  }
+  setState(value) {
+    this._state = value;
+  }
+  getState() {
+    return this._state;
+  }
+}
+const state = new StateManager();
+
+const redirectHandler = () => location.assign(state.getState());
+
+const linkInputHandler = () => {
+  state.setState(prompt('Enter link for redirect', 'http://google.com'));
+
+  if (!state.getState()) {
+    redirectBtn.toggleAttribute('disabled');
+    return;
+  }
+
+  redirectBtn.removeAttribute('disabled');
+};
+
+linkBtn.addEventListener('click', linkInputHandler);
+redirectBtn.addEventListener('click', redirectHandler);
