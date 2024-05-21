2d4b185a7534f172e98b9df34b9e4954760697f2
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-05-21 15:12:53 +0200

    hw_35

---
+++ Homework_35/build/homework.js
@@ -0,0 +1,29 @@
+"use strict";
+class Coach {
+    #name;
+    #specialization;
+    #rating;
+    constructor(name, specialization, rating) {
+        this.#name = name;
+        this.#specialization = specialization;
+        this.#rating = rating;
+    }
+    displayInfo() {
+        return `Coach: ${this.#name}, Specialization: ${this.#specialization}, Rating: ${this.#rating}`;
+    }
+    get name() {
+        return this.#name;
+    }
+    get specialization() {
+        return this.#specialization;
+    }
+    get rating() {
+        return this.#rating;
+    }
+}
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+console.log(coach1.displayInfo()); // "Coach: John Doe, Specialization: Fitness, Rating: 4.7"
+console.log(coach1.name, coach1.rating, coach1.specialization);
+console.log(coach2.displayInfo()); // "Coach: Alice Smith, Specialization: Yoga, Rating: 4.9"
+console.log(coach2.name, coach2.rating, coach2.specialization);
---
+++ Homework_35/homework.ts
@@ -0,0 +1,33 @@
+class Coach {
+  #name: string;
+  #specialization: string;
+  #rating: number;
+
+  constructor(name: string, specialization: string, rating: number) {
+    this.#name = name;
+    this.#specialization = specialization;
+    this.#rating = rating;
+  }
+  displayInfo(): string {
+    return `Coach: ${this.#name}, Specialization: ${
+      this.#specialization
+    }, Rating: ${this.#rating}`;
+  }
+  get name(): string {
+    return this.#name;
+  }
+  get specialization(): string {
+    return this.#specialization;
+  }
+  get rating(): number {
+    return this.#rating;
+  }
+}
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+console.log(coach1.displayInfo()); // "Coach: John Doe, Specialization: Fitness, Rating: 4.7"
+console.log(coach1.name, coach1.rating, coach1.specialization);
+console.log(coach2.displayInfo()); // "Coach: Alice Smith, Specialization: Yoga, Rating: 4.9"
+console.log(coach2.name, coach2.rating, coach2.specialization);
