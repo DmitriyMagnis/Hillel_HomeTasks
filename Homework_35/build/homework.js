e787a2fe109fe69b52cdad14db4bebb08c624dfb
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-05-21 15:15:13 +0200

    hw_35/2

--- Homework_35/build/homework.js
+++ Homework_35/build/homework.js
@@ -25,5 +25,5 @@ const coach1 = new Coach('John Doe', 'Fitness', 4.7);
 const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
 console.log(coach1.displayInfo()); // "Coach: John Doe, Specialization: Fitness, Rating: 4.7"
 console.log(coach1.name, coach1.rating, coach1.specialization);
-console.log(coach2.displayInfo()); // "Coach: Alice Smith, Specialization: Yoga, Rating: 4.9"
+console.log(coach2.displayInfo()); // "Coach: Alice Smith, Specialization: Yoga, Rating:4.9"
 console.log(coach2.name, coach2.rating, coach2.specialization);
